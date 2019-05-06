a030ebe4cbd2548ab47265f1224906157533e8f8
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-06 23:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  let name= you
-  return 'happyHolidays ${name}!'
+  let name = you
+  return 'happyHolidays, ${name}!'
 }
